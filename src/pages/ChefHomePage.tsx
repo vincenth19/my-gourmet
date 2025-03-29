7a954b778b2e834f4b4f8ddb1fe6e0836151a90a
Vincent Haryadi <vincenthary19@gmail.com>
2025-03-29 23:40:21 +1000

    hide view toggle

--- src/pages/ChefHomePage.tsx
+++ src/pages/ChefHomePage.tsx
@@ -623,7 +623,7 @@ const ChefHomePage = () => {
   return (
     <div className="min-h-screen bg-gray-50">
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <ViewToggle view={view} setView={setView} />
+        {/* <ViewToggle view={view} setView={setView} /> */}
         
         {loading ? (
           <LoadingState />
